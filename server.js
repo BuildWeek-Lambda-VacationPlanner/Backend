7b39a2db3ff5fe90ee19a46304f34019eb2b69b4
Jamison Blackwell <blackwellj1040@gmail.com>
2019-09-20 12:21:28 -0400

    added a root endpoint to see if heroku is working

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-
+const Users = require('./users/users-model.js');
 const authenticate = require('./auth/authenticate-middleware.js');
 const UserRouter = require('./users/users-router.js');
 const VacationRouter = require('./vacations/vacation-router.js');
@@ -14,5 +14,15 @@ server.use(express.json());
 
 server.use('/api/users', UserRouter);
 server.use('/api/vacations', VacationRouter);
+
+server.get('/', (req, res) => {
+    Users.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err =>
+        console.log(err))
+
+})
 
 module.exports = server;
